e8fa5f2b397452361a622b44d202573359712cac
gazwald <gary.brandon@gmail.com>
2017-09-02 13:54:58 +1000

    DB creation script

---
+++ create.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import configparser
+from sqlalchemy import create_engine
+
+from database.orm import Base
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+dbstring = '{provider}://{username}:{password}@{host}:{port}/{database}'
+engine = create_engine(dbstring.format(provider=config['database'].get('provider', 'postgresql'),
+                                       username=config['database']['username'],
+                                       password=config['database']['password'],
+                                       host=config['database']['host'],
+                                       port=config['database']['port'],
+                                       database=config['database']['database']))
+Base.metadata.create_all(engine)
