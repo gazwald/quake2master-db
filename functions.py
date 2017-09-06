b02545143fc2bc16843117964928b079ce0b0b40
gazwald <gary.brandon@gmail.com>
2017-09-07 09:24:45 +1000

    Created generic function for handling database connections

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import configparser
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 def get_or_create(session, model, **kwargs):
@@ -15,3 +19,17 @@ def get_or_create(session, model, **kwargs):
         session.add(instance)
         session.commit()
         return instance
+
+
+def create_session():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    dbstring = '{provider}://{username}:{password}@{host}:{port}/{database}'
+    engine = create_engine(dbstring.format(provider=config['database'].get('provider', 'postgresql'),
+                                           username=config['database']['username'],
+                                           password=config['database']['password'],
+                                           host=config['database']['host'],
+                                           port=config['database']['port'],
+                                           database=config['database']['database']))
+    Session = sessionmaker(bind=engine)
+    return Session()
