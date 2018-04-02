622adb83e34cb14c67f4d116056e51d946e76503
gazwald <gary.brandon@gmail.com>
2018-04-02 14:58:34 +1000

    Updating create.py to use new config and create_db_conn

--- create.py
+++ create.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 import configparser
-from sqlalchemy import create_engine
 
 from orm import Base
+from functions import create_db_conn
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-dbstring = '{provider}://{username}:{password}@{host}:{port}/{database}'
-engine = create_engine(dbstring.format(provider=config['database'].get('provider', 'postgresql'),
-                                       username=config['database']['username'],
-                                       password=config['database']['password'],
-                                       host=config['database']['host'],
-                                       port=config['database']['port'],
-                                       database=config['database']['database']))
+engine = create_db_conn(config['general'].get('environment', 'testing'))
 Base.metadata.create_all(engine)
